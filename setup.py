5f5bf17932690e0dd116cfbf8908c12c3f4cc33f
Neo Chan <netkiller@msn.com>
2013-08-16 12:56:17 +0800

    Create setup.py
    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name = 'nodekeeper',
+    version = '1.0.0',
+    py_modules = ['nodekeeper'],
+    author = 'neo.chen',
+    author_email = 'netkiller@msn.com',
+    description = 'nodekeeper',
+    url = 'http://netkiller.github.io',
+)
